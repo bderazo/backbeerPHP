0c6c24ed5b89d56117cd07ec20f1210d3e16e286
bderazo <bderazo@espe.edu.ec>
2023-06-07 12:16:53 -0500

    agregar & en el envio de correos

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -8,7 +8,7 @@
 <body style="width: 600px; margin: 0 auto; text-align: center;">
     <h1>Hola, usuario OnlyTap</h1>
     <p>Se solicitó un restablecimiento de contraseña para tu cuenta {{ $correo }}, haz clic en el botón que aparece a continuación para cambiar tu contraseña</p>
-    <a href="https://onlytap.proatek.com/reset/token={{ $token }}?email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a>
+    <a href="https://onlytap.proatek.com/reset/token={{ $token }}&email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a>
     <p>Si tú no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje.</p>
     <p>Este enlace solo es válido dentro de los próximos 60 minutos</p>
     <p>¡Gracias por usar nuestra aplicación!</p>
@@ -18,7 +18,7 @@
 <br>
 <div style="width: 600px; margin: 0 auto; text-align: center;">
     <p>
-        Si tiene problemas para hacer clic en el botón "Cambiar contraseña", copie y pegue la URL a continuación en su navegador web: <a href="https://onlytap.proatek.com/reset/token={{ $token }}?email={{$correo}}" target="_blank">https://onlytap.proatek.com/reset/token={{ $token }}?email={{$correo}}</a></p>
+        Si tiene problemas para hacer clic en el botón "Cambiar contraseña", copie y pegue la URL a continuación en su navegador web: <a href="https://onlytap.proatek.com/reset/token={{ $token }}&email={{$correo}}" target="_blank">https://onlytap.proatek.com/reset/token={{ $token }}&email={{$correo}}</a></p>
     </p>
 </div>
 
--- resources/views/emails/reset-passwordP.blade.php
+++ resources/views/emails/reset-passwordP.blade.php
@@ -8,8 +8,8 @@
 <body style="width: 600px; margin: 0 auto; text-align: center;">
     <h1>Hola, usuario Proatek</h1>
     <p>Se solicitó un restablecimiento de contraseña para tu cuenta {{ $correo }}, haz clic en el botón que aparece a continuación para cambiar tu contraseña</p>
-    <!-- <a href="https://proatek.com/session/reset-password?token={{ $token }}?email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a> -->
-    <a href="http://localhost:3000/session/reset-password?token={{ $token }}&email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a>
+    <a href="https://proatek.com/session/reset-password?token={{ $token }}?email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a>
+    <!-- <a href="http://localhost:3000/session/reset-password?token={{ $token }}&email={{$correo}}" style="background-color: #2D3747; color: #fff; padding: 10px 20px; text-decoration: none; display: inline-block;">Cambiar contraseña</a> -->
     <p>Si tú no realizaste la solicitud de cambio de contraseña, solo ignora este mensaje.</p>
     <p>Este enlace solo es válido dentro de los próximos 60 minutos</p>
     <p>¡Gracias por usar nuestra aplicación!</p>
